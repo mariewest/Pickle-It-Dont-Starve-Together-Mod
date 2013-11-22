0698a8d03234c97dad999a9919531be25f01ba07
Nick West <nickkwest@gmail.com>
2013-11-22 10:44:30 -0800

    Inventory images for new veggies & pig foot

---
+++ scripts/prefabs/pig_foot.lua
@@ -0,0 +1,6 @@
+local assets=
+{
+    Asset("ANIM", "anim/pig_foot.zip"),						-- Animation Zip
+    Asset("ATLAS", "images/inventoryimages/pig_foot.xml"),	-- Atlas for inventory TEX
+    Asset("IMAGE", "images/inventoryimages/pig_foot.tex"),	-- TEX for inventory
+}
