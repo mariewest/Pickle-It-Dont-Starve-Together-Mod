dc31dfc8ef0cae766c3e5c68ef1505a92602cf3d
Marie West <marie@mariewest.com>
2015-03-09 18:51:52 -0700

    Updated modinfo and README

--- modinfo.lua
+++ modinfo.lua
@@ -7,8 +7,8 @@ Art by: Sean Closson (seanclosson.com)
 name = "Pickle It - Git"
 description = "Pickle your foods to make them last longer.\nPickle It adds a pickle barrel, pickled foods, and some new veggies.\n\nArt by: Sean Closson"
 author = "Marie and Nick West"
-version = "1.3.1"
-forumthread = "/topic/29559-wip-pickle-it-a-mod-that-lets-you-pickle-things/"
+version = "1.3.2"
+forumthread = "/files/file/1123-pickle-it-dst-version/"
 --forumthread = "/files/file/392-pickle-it/"
 
 standalone = false
